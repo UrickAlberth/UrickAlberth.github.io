e6b94160a0fba9cdf6b0a3720fe413250a4326bf
Urick Alberth <109443000+UrickAlberth@users.noreply.github.com>
2023-10-21 11:25:54 -0300

    adicionado projetos.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const projetos = {
   0: {
+    nome: "Jogo da Memória - Desafie sua Memória!:",
+    descricao:
+      "Este projeto é um jogo clássico da memória implementado com HTML, CSS e JavaScript. O objetivo do jogo é encontrar todos os pares de cartas iguais em um tabuleiro virando-as uma de cada vez. Cada vez que duas cartas são viradas, elas são comparadas. Se forem iguais, elas permanecem viradas e o jogador ganha pontos. Caso contrário, as cartas são desviradas e o jogador perde pontos. O jogo termina quando todos os pares são encontrados.",
+    projetoUrl: "https://urickalberth.github.io/jogo_da_memoria/",
+    imgUrl: "https://urickalberth.github.io/jogo_da_memoria/jogo_da_memoria.png"
+  },
+    1: {
+    nome: "Gomoku:",
+    descricao:
+      "Este projeto é um jogo coreano chamado Gomoku, cujo objetivo é colocar cinco peças em sequência primeiro. Ele envolve uma grade em que os jogadores alternam entre colocar suas peças em posições vazias. O jogo verifica constantemente se alguém conseguiu alinhar cinco peças e declara o vencedor quando isso acontece.",
+    projetoUrl: "https://urickalberth.github.io/gomoku/",
+    imgUrl: "https://urickalberth.github.io/gomoku/gomoku.png"
+  },
+  2: {
     nome: "Tabela Periódica Interativa:",
     descricao:
       "Desenvolvido um projeto de tabela periódica interativa com elementos representados como objetos em um array. A tabela é responsiva, colorida e exibe os elementos em ordem. Os usuários podem explorar e interagir com os elementos de forma intuitiva.",
@@ -7,35 +21,42 @@ const projetos = {
     imgUrl:
       "https://urickalberth.github.io/tabela_periodica/tabela_periodica.png"
   },
-  1: {
+  3: {
     nome: "Jogo da Velha Inteligente:",
     descricao:
       "Criei um jogo da velha com opções para jogar contra um adversário humano ou computador. A inteligência artificial possui diferentes níveis de dificuldade: fácil, médio e difícil. Além disso, o jogo calcula e exibe a pontuação de cada jogador a cada vitória.",
     projetoUrl: "https://urickalberth.github.io/jogo_da_velha/",
     imgUrl: "https://urickalberth.github.io/jogo_da_velha/jogo_da_velha.png"
   },
-  2: {
+  4: {
+    nome: "Jogo da Velha 2:",
+    descricao:
+      "Neste projeto, eu criei uma variação interessante do clássico jogo da velha. Nele, a grade tradicional de 3x3 é dividida em nove divisões menores, cada uma contendo seu próprio jogo da velha. Os jogadores competem nos pequenos jogos da velha, e quem vencer em um desses pequenos jogos pode marcar sua posição nas divisões maiores. A estratégia se torna mais complexa, já que a jogada nos pequenos jogos afeta o jogo nas divisões grandes.",
+    projetoUrl: "https://urickalberth.github.io/jogo_da_velha2/",
+    imgUrl: "https://urickalberth.github.io/jogo_da_velha2/jogo_da_velha2.png"
+  },
+  5: {
     nome: "Jogo da Forca Interativo:",
     descricao:
       "Desenvolvido um jogo da forca com um teclado virtual que permite ao jogador escolher as letras das tentativas. O jogo exibe quais letras já foram usadas e renderiza partes do corpo do boneco a cada erro. A pontuação varia de acordo com o número de erros, e o recorde é armazenado no local storage.",
     projetoUrl: "https://urickalberth.github.io/jogo_da_forca/",
     imgUrl: "https://urickalberth.github.io/jogo_da_forca/jogo_da_forca.png"
   },
-  3: {
+  6: {
     nome: "Documentação do JavaScript (não oficial):",
     descricao:
       "Criei um site contendo a documentação do JavaScript, não oficial, com uma barra de menu vertical responsiva para fácil navegação. A documentação abrange diversos tópicos do JavaScript, auxiliando desenvolvedores a encontrar informações relevantes.",
     projetoUrl: "https://urickalberth.github.io/doc_js/",
     imgUrl: "https://urickalberth.github.io/doc_js/doc_js.png"
   },
-  4: {
+  7: {
     nome: "Bloco de Notas Avançado:",
     descricao:
       "Criei um bloco de anotações com recursos avançados, permitindo a inserção, edição e exclusão de notas. As notas são armazenadas no indexDB, e o usuário pode criar notas com códigos HTML renderizados ou não, facilitando o registro de informações formatadas.",
     projetoUrl: "https://urickalberth.github.io/anotacoes/",
     imgUrl: "https://urickalberth.github.io/anotacoes/anotacoes.png"
   },
-  5: {
+  8: {
     nome: "Lista de Tarefas Eficiente:",
     descricao:
       "Desenvolvi uma lista de tarefas eficiente, que permite ao usuário inserir, editar, excluir e marcar como concluídas as tarefas. As tarefas são salvas no local storage, garantindo que não sejam perdidas mesmo após o fechamento do navegador.",
@@ -43,63 +64,63 @@ const projetos = {
     imgUrl:
       "https://urickalberth.github.io/lista_de_tarefas/lista_de_tarefas.png"
   },
-  6: {
+  9: {
     nome: "Réplica do Perfil no Codepen:",
     descricao:
       "Criei uma réplica da página do meu perfil no Codepen, exibindo meus projetos, snippets e informações relevantes sobre meu trabalho como desenvolvedor.",
     projetoUrl: "https://urickalberth.github.io/perfil_codepen/",
     imgUrl: "https://urickalberth.github.io/perfil_codepen/perfil_codepen.png"
   },
-  7: {
+  10: {
     nome: "Teste de Preferências de Carreira:",
     descricao:
       "Desenvolvi um site de teste que ajuda os usuários a descobrir suas preferências de carreira. O questionário avalia as respostas e calcula os resultados, exibindo a pontuação para cada resultado.",
     projetoUrl: "https://urickalberth.github.io/teste_ancoras/",
     imgUrl: "https://urickalberth.github.io/teste_ancoras/teste_ancoras.png"
   },
-  8: {
+  11: {
     nome: "Calculadora Multifuncional:",
     descricao:
       "Criei uma calculadora multifuncional que permite ao usuário realizar cálculos utilizando tanto o teclado virtual quanto o teclado do PC. A calculadora possui várias funções matemáticas e opções adicionais para atender às necessidades dos usuários.",
     projetoUrl: "https://urickalberth.github.io/calculadora/",
     imgUrl: "https://urickalberth.github.io/calculadora/calculadora.png"
   },
-  9: {
+  12: {
     nome: "Protótipo Web MindEles (Saúde Mental):",
     descricao:
       "Desenvolvi um aplicativo para cadastro de pacientes, agendamento de avaliações psicológicas e monitoramento do período de validade dessas avaliações. O aplicativo envia alertas automáticos quando uma avaliação está próxima do vencimento e oferece testes online para avaliação da saúde mental dos pacientes.",
     projetoUrl: "https://urickalberth.github.io/mindeles1/",
     imgUrl: "https://urickalberth.github.io/mindeles1/mindeles.png"
   },
-  10: {
+  13: {
     nome: "Sorteador Online:",
     descricao:
       "Criei um site para sorteios aleatórios, permitindo que os usuários escolham a quantidade de sorteios e o número de pessoas sorteadas. O sistema realiza o sorteio de forma justa e transparente.",
     projetoUrl: "https://urickalberth.github.io/sorteador/",
     imgUrl: "https://urickalberth.github.io/sorteador/sorteador.png"
   },
-  11: {
+  14: {
     nome: "Protótipo Web Tocaki (Encontro de Músicos e Eventos):",
     descricao:
       "Desenvolvi um aplicativo que permite o cadastro de empresas, músicos e eventos, facilitando o encontro entre músicos e organizadores de eventos. Os usuários podem encontrar oportunidades musicais e eventos relevantes para suas carreiras.",
     projetoUrl: "https://urickalberth.github.io/tocaki/",
     imgUrl: "https://urickalberth.github.io/tocaki/tocaki.png"
   },
-  12: {
+  15: {
     nome: "Xadrez Interativo:",
     descricao:
       "Criei um jogo de xadrez contra um adversário humano com uma interface amigável e intuitiva. O tabuleiro destaca os movimentos possíveis quando clicado em cada peça, e os usuários têm a opção de escolher a cor do tabuleiro e das peças para personalização.",
     projetoUrl: "https://urickalberth.github.io/xadrez/",
     imgUrl: "https://urickalberth.github.io/xadrez/xadrez.png"
   },
-  13: {
+  16: {
     nome: "Gerador de QR-CODE:",
     descricao:
       "Desenvolvi um gerador de QR-CODE que utiliza uma API para gerar códigos QR a partir de textos ou links inseridos. Os usuários podem personalizar a cor e o tamanho do QR-CODE de acordo com suas preferências.",
     projetoUrl: "https://urickalberth.github.io/gerador_qr_code/",
     imgUrl: "https://urickalberth.github.io/gerador_qr_code/gerador_qr_code.png"
   },
-  14: {
+  17: {
     nome: "Relógio Analógico:",
     descricao:
       "Criei um projeto com relógios analógico e digital, exibindo a hora atual de forma clara e precisa. Os relógios são sincronizados com o horário do sistema e oferecem uma experiência visual agradável.",
@@ -107,20 +128,28 @@ const projetos = {
     imgUrl:
       "https://urickalberth.github.io/relogio_analogico/relogio_analogico.png"
   },
-  15: {
+  18: {
     nome: "Relógio Digital:",
     descricao:
       "Criei um projeto com relógios analógico e digital, exibindo a hora atual de forma clara e precisa. Os relógios são sincronizados com o horário do sistema e oferecem uma experiência visual agradável.",
     projetoUrl: "https://urickalberth.github.io/relogio_digital/",
     imgUrl: "https://urickalberth.github.io/relogio_digital/relogio_digital.png"
   },
-  16: {
-    nome: "Jogo da Memória - Desafie sua Memória!:",
+    19: {
+    nome: "Ping Pong Retrô:",
     descricao:
-      "Este projeto é um jogo clássico da memória implementado com HTML, CSS e JavaScript. O objetivo do jogo é encontrar todos os pares de cartas iguais em um tabuleiro virando-as uma de cada vez. Cada vez que duas cartas são viradas, elas são comparadas. Se forem iguais, elas permanecem viradas e o jogador ganha pontos. Caso contrário, as cartas são desviradas e o jogador perde pontos. O jogo termina quando todos os pares são encontrados.",
-    projetoUrl: "https://urickalberth.github.io/jogo_da_memoria/",
-    imgUrl: "https://urickalberth.github.io/jogo_da_memoria/jogo_da_memoria.png"
-  }
+      "Este é um jogo clássico de pingue-pongue em que os jogadores controlam as raquetes usando o mouse. O objetivo é rebater a bola de um lado para o outro e evitar que ela ultrapasse a raquete do jogador. A velocidade da bola pode aumentar à medida que o jogo avança, tornando-o desafiador e divertido.",
+    projetoUrl: "https://urickalberth.github.io/pingpong/",
+    imgUrl: "https://urickalberth.github.io/pingpong/pingpong.png"
+  },
+    20: {
+    nome: "Snake (Cobrinha):",
+    descricao:
+      "No projeto Snake, eu implementei o famoso jogo da cobrinha. Os jogadores controlam uma cobra que cresce à medida que consome itens no cenário. O objetivo é evitar colidir com as bordas do jogo ou com o próprio corpo da cobra. À medida que a cobra cresce, o desafio aumenta, tornando-o um jogo clássico e viciante.",
+    projetoUrl: "https://urickalberth.github.io/snake/",
+    imgUrl: "https://urickalberth.github.io/snake/snake.png"
+  },
+
 };
 // Função para manipular o menu hamburguer
 // Função para alternar a visibilidade do menu
